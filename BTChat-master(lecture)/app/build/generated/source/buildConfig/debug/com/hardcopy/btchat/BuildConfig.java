0228a14f4ea91b2a3980ca21858fb3d1b531fc2e
DESKTOP-0UDSOHF\ajou <vivi9novia@gmail.com>
2016-09-13 00:44:23 +0900

    ppt week2

---
+++ BTChat-master(lecture)/app/build/generated/source/buildConfig/debug/com/hardcopy/btchat/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.hardcopy.btchat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.hardcopy.btchat";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.0.2";
+}
